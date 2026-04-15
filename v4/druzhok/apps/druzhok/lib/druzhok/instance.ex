71ccd1b2d2021c9601249126562917611f291bda
igor <igor.n.kuz@gmail.com>
2026-04-15 22:37:41 +0500

    schema: add group_sessions_per_user field + cast

--- v4/druzhok/apps/druzhok/lib/druzhok/instance.ex
+++ v4/druzhok/apps/druzhok/lib/druzhok/instance.ex
@@ -34,6 +34,7 @@ defmodule Druzhok.Instance do
     field :heartbeat_target, :string
     field :fallback_models, :string
     field :dreaming, :boolean, default: false
+    field :group_sessions_per_user, :boolean, default: true
 
     has_one :budget, Druzhok.Budget
 
@@ -42,7 +43,7 @@ defmodule Druzhok.Instance do
 
   def changeset(instance, attrs) do
     instance
-    |> cast(attrs, [:name, :telegram_token, :model, :workspace, :active, :heartbeat_interval, :owner_telegram_id, :sandbox, :timezone, :api_key, :daily_token_limit, :dream_hour, :language, :tenant_key, :bot_runtime, :on_demand_model, :mention_only, :reject_message, :welcome_message, :allowed_telegram_ids, :allowed_telegram_chats, :allow_all_telegram_users, :trigger_name, :image_model, :audio_model, :embedding_model, :heartbeat_active_start, :heartbeat_active_end, :heartbeat_target, :fallback_models, :dreaming])
+    |> cast(attrs, [:name, :telegram_token, :model, :workspace, :active, :heartbeat_interval, :owner_telegram_id, :sandbox, :timezone, :api_key, :daily_token_limit, :dream_hour, :language, :tenant_key, :bot_runtime, :on_demand_model, :mention_only, :reject_message, :welcome_message, :allowed_telegram_ids, :allowed_telegram_chats, :allow_all_telegram_users, :trigger_name, :image_model, :audio_model, :embedding_model, :heartbeat_active_start, :heartbeat_active_end, :heartbeat_target, :fallback_models, :dreaming, :group_sessions_per_user])
     |> validate_required([:name, :model, :workspace])
     |> unique_constraint(:name)
   end
